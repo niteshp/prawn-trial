f6fdd8642e6f586784a660b0472b96bf4d1f00c4
Nitesh Patel <nitesh@scriptyx.com>
2018-02-25 20:45:26 -0800

    initial commit

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    respond_to do |format|
+    	format.html
+    	format.pdf do
+    		pdf = OrderPdf.new(@order)
+    		send_data pdf.render,
+    				  filename: "order_#{ @order.order_number }",
+    				  type: 'application/pdf',
+    				  disposition: 'inline'
+    	end
+    end
+  end
+end
---
+++ app/models/line_item.rb
@@ -0,0 +1,7 @@
+class LineItem < ApplicationRecord
+  belongs_to :order
+
+  def full_price
+    unit_price * quantity
+  end
+end
