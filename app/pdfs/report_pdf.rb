88bbd8abb5a27e77717203fbf1a807697c055047
Nitesh Patel <nitesh@scriptyx.com>
2018-03-03 17:23:57 -0800

    Adding page 1 content ... WIP

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -10,6 +10,31 @@ class ReportPdf < Prawn::Document
         start_new_page
     end
 
+    repeat([1]) do
+
+        # header
+        bounding_box [bounds.left, bounds.top], :width  => bounds.width do
+            stroke_bounds
+            font "Helvetica"
+
+            text "<b>Customer Name:</b>  #{ customer_name(@report) }", 
+                 align: :right,
+                 size: 10,
+                 inline_format: true,
+                 position: :right
+            text "<b>Test ID:</b>  #{ test_id(@report) }", 
+                 align: :right,
+                 size: 10,
+                 inline_format: true,
+                 position: :right
+            image "#{Rails.root}/app/assets/images/logo.png", width: 150, position: :left
+            
+            stroke_horizontal_rule
+        end
+
+      text "Page 1"
+    end
+
     repeat(2..self.page_count) do
         move_down 50
 
@@ -30,11 +55,20 @@ class ReportPdf < Prawn::Document
         # footer
         bounding_box [bounds.left, bounds.bottom + 50], :width  => bounds.width do
             font "Helvetica"
+            stroke_horizontal_rule
             move_down(5)
-            text "<b>Dna4Life reports do not provide medical advice, diagnosis, or treatment. The report is for informational purposes only, and should not be interpreted as specific professional medical advice. Please consult with a doctor or qualified healthcare professional before making decisions about medical conditions, or before starting and stopping any treatment, diet or exercise program.</b>",
+            text "<b>Clinsyte reports do not provide medical advice, diagnosis, or treatment. " \
+                 "The report is for informational purposes only, and should not be interpreted" \
+                 " as specific professional medical advice. Please consult with a doctor or qu" \
+                 "alified healthcare professional before making decisions about medical condit" \
+                 "ions, or before starting and stopping any treatment, diet or exercise progra" \
+                 "m.</b>",
                  size: 7,
                  inline_format: true
-            stroke_horizontal_rule
+            move_down 13
+            text "</i>© Copyright Scriptyx, 2015. United States Patent Pending. Joint Venture Group, LLC. All rights reserved.</i>",
+                 size: 7,
+                 inline_format: true
         end
       end
 
